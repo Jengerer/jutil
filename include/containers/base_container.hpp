32bb9413952cb7bfdf563be504e2f035c81d2e94
U-AMD\ebabinet <ebabinet@CNEBABINETW7.amd.com>
2012-08-13 16:00:39 -0400

    Removed base container CPP, changed base container to template.

--- include/containers/base_container.hpp
+++ include/containers/base_container.hpp
@@ -9,14 +9,21 @@ namespace JUTIL
 
     /*
      * Generic container class with common container functions.
+     * This base class is just for the base set of functions; an inheriting
+     * class should NOT be referred to or deleted by a BaseContainer* pointer,
+     * since there is no virtual destructor.
      */
+    template <class Type, class Allocator>
     class BaseContainer : public BaseClass
     {
 
     public:
 
-        BaseContainer( AllocatorInterface* allocator = BaseClassServices::get_default_allocator() );
-        virtual ~BaseContainer( void );
+        BaseContainer( void );
+        ~BaseContainer( void );
+
+        // Memory management.
+        bool reserve( size_t elements );
 
     protected:
 
@@ -25,10 +32,46 @@ namespace JUTIL
 
     private:
 
-        
-        AllocatorInterface* allocator_;
+        Allocator allocator_;
 
     };
+
+    /*
+     * Base container constructor.
+     */
+    template <class Type, class Allocator>
+    BaseContainer<Type, Allocator>::BaseContainer( AllocatorInterface* allocator )
+    {
+        set_allocator( allocator );
+    }
+
+    /*
+     * Base container destructor.
+     */
+    template <class Type, class Allocator>
+    BaseContainer<Type, Allocator>::~BaseContainer( void )
+    {
+    }
+
+    /*
+     * Setting new container object allocator.
+     * Allocator must have been initialized before setting; no error
+     * checking is done here.
+     */
+    template <class Type, class Allocator>
+    void BaseContainer<Type, Allocator>::set_allocator( AllocatorInterface* allocator )
+    {
+        allocator_ = allocator;
+    } 
+
+    /*
+     * Get current allocator.
+     */
+    template <class Type, class Allocator>
+    AllocatorInterface* BaseContainer<Type, Allocator>::get_allocator( void ) const
+    {
+        return allocator_;
+    }
 
 }
 
