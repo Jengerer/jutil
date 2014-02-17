0886d042968efa2000f0d0c298342e575a98b575
U-Jengerer-PC\Jengerer <Jengerer@Jengerer-PC.(none)>
2014-02-17 14:56:04 -0500

    Added missing version header.

---
+++ include/jutil_version.hpp
@@ -0,0 +1,11 @@
+#ifndef JUTIL_VERSION_HPP
+#define JUTIL_VERSION_HPP
+
+#include <string/string.hpp>
+
+namespace JUTIL
+{
+	const ConstantString JUTIL_VERSION( "0.1" );
+}
+
+#endif // JUTIL_VERSION_HPP
